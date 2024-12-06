8420919bf3c134b792947b2f6b373e563960dfa2
matjoeee <wuytsma@protonmail.com>
2024-12-06 12:43:52 +0100

    Day 5 initialized

---
+++ Day 5/first.py
@@ -0,0 +1,7 @@
+""" 
+First part of the Advent of Code exercise
+
+Day 5 --> https://adventofcode.com/2024/day/5
+
+Made by Matjoeee
+"""
---
+++ Day 5/second.py
@@ -0,0 +1,7 @@
+""" 
+Second part of the Advent of Code exercise
+
+Day 5 --> https://adventofcode.com/2024/day/5#part2
+
+Made by Matjoeee
+"""
