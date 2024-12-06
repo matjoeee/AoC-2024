d71808e051a1b9ca75b6cd9126d08562274e117a
matjoeee <wuytsma@protonmail.com>
2024-12-06 12:46:26 +0100

    Day 6 initialized

---
+++ Day 6/first.py
@@ -0,0 +1,7 @@
+""" 
+First part of the Advent of Code exercise
+
+Day 6 --> https://adventofcode.com/2024/day/6
+
+Made by Matjoeee
+"""
---
+++ Day 6/second.py
@@ -0,0 +1,7 @@
+""" 
+Second part of the Advent of Code exercise
+
+Day 6 --> https://adventofcode.com/2024/day/6#part2
+
+Made by Matjoeee
+"""
