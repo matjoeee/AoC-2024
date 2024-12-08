e88d188bf554aab973602a99b65b1247f7575695
matjoeee <wuytsma@protonmail.com>
2024-12-08 12:33:20 +0100

    Day 8 initialized

---
+++ Day 8/first.py
@@ -0,0 +1,7 @@
+""" 
+First part of the Advent of Code exercise
+
+Day 8 --> https://adventofcode.com/2024/day/8
+
+Made by Matjoeee
+"""
---
+++ Day 8/second.py
@@ -0,0 +1,7 @@
+""" 
+Second part of the Advent of Code exercise
+
+Day 8 --> https://adventofcode.com/2024/day/8#part2
+
+Made by Matjoeee
+"""
