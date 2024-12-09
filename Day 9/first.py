a7a7cef1c098210c20b463de9452980edda24034
matjoeee <wuytsma@protonmail.com>
2024-12-09 13:22:46 +0100

    Day 9 initialized

---
+++ Day 9/first.py
@@ -0,0 +1,7 @@
+""" 
+First part of the Advent of Code exercise
+
+Day 9 --> https://adventofcode.com/2024/day/9
+
+Made by Matjoeee
+"""
---
+++ Day 9/second.py
@@ -0,0 +1,7 @@
+""" 
+Second part of the Advent of Code exercise
+
+Day 9 --> https://adventofcode.com/2024/day/9#part2
+
+Made by Matjoeee
+"""
