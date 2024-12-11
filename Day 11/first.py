47ce232c478fb19bdd7595be5d27d746c33845bc
matjoeee <wuytsma@protonmail.com>
2024-12-11 18:29:17 +0100

    Day 11 initialized

---
+++ Day 11/first.py
@@ -0,0 +1,7 @@
+""" 
+First part of the Advent of Code exercise
+
+Day 11 --> https://adventofcode.com/2024/day/11
+
+Made by Matjoeee
+"""
---
+++ Day 11/second.py
@@ -0,0 +1,7 @@
+""" 
+Second part of the Advent of Code exercise
+
+Day 11 --> https://adventofcode.com/2024/day/11#part2
+
+Made by Matjoeee
+"""
