6e50e6c96c3f546289910c90ea09dfba37871ef5
matjoeee <wuytsma@protonmail.com>
2024-12-11 18:27:39 +0100

    Day 10 initialized

---
+++ Day 10/first.py
@@ -0,0 +1,7 @@
+""" 
+First part of the Advent of Code exercise
+
+Day 10 --> https://adventofcode.com/2024/day/10
+
+Made by Matjoeee
+"""
---
+++ Day 10/second.py
@@ -0,0 +1,7 @@
+""" 
+Second part of the Advent of Code exercise
+
+Day 10 --> https://adventofcode.com/2024/day/10#part2
+
+Made by Matjoeee
+"""
