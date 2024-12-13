b01021b822ef851adfbebad8fd67463bcc62a7ff
matjoeee <wuytsma@protonmail.com>
2024-12-13 11:24:47 +0100

    Day 13 initialized

---
+++ Day 13/first.py
@@ -0,0 +1,7 @@
+""" 
+First part of the Advent of Code exercise
+
+Day 13 --> https://adventofcode.com/2024/day/13
+
+Made by Matjoeee
+"""
---
+++ Day 13/second.py
@@ -0,0 +1,7 @@
+""" 
+Second part of the Advent of Code exercise
+
+Day 13 --> https://adventofcode.com/2024/day/13#part2
+
+Made by Matjoeee
+"""
